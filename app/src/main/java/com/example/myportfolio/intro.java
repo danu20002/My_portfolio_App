e9b1e7dbf15b4108902c5673ea9924d39f0fd03c
Danesh Naik <naikdanesh2@gmail.com>
2023-10-23 06:41:52 +0530

    pushed

--- app/src/main/java/com/example/myportfolio/intro.java
+++ app/src/main/java/com/example/myportfolio/intro.java
@@ -2,7 +2,10 @@ package com.example.myportfolio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class intro extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+
     }
 }
